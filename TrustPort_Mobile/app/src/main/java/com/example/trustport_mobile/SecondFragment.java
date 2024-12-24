4b2b779dd7829320cbb153a9cb86f6b1001a8184
MinhHien <150352316+DMinhHien@users.noreply.github.com>
2024-12-24 20:34:49 +0700

    Change project

--- TrustPort_Mobile/app/src/main/java/com/example/trustport_mobile/SecondFragment.java
+++ TrustPort_Mobile/app/src/main/java/com/example/trustport_mobile/SecondFragment.java
@@ -13,32 +13,14 @@ import com.example.trustport_mobile.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
 
-    private FragmentSecondBinding binding;
-
-    @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-
-    }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        binding.buttonSecond.setOnClickListener(v ->
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment)
-        );
+    public SecondFragment(){
+        // require a empty public constructor
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_second, container, false);
     }
 
 }
--- TrustPort_Mobile/app/src/main/java/com/example/trustport_mobile/ThirdFragment.java
+++ TrustPort_Mobile/app/src/main/java/com/example/trustport_mobile/ThirdFragment.java
@@ -5,22 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.trustport_mobile.databinding.FragmentFirstBinding;
+public class ThirdFragment extends Fragment {
 
-public class FirstFragment extends Fragment {
-
-    public FirstFragment(){
+    public ThirdFragment(){
         // require a empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return inflater.inflate(R.layout.fragment_third, container, false);
     }
 
 
