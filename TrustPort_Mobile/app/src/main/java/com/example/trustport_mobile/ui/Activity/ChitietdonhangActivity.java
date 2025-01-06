918251050757fd262b7da7bdea858b10ab9b4b6b
duongtx <duongtx.t2.1922@gmail.com>
2025-01-06 08:40:13 +0700

    chitietdon

---
+++ TrustPort_Mobile/app/src/main/java/com/example/trustport_mobile/ui/Activity/ChitietdonhangActivity.java
@@ -0,0 +1,14 @@
+package com.example.trustport_mobile.ui.Activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.trustport_mobile.R;
+
+public class ChitietdonhangActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chitiethanghoa);
+    }
+}
--- TrustPort_Mobile/app/src/main/java/com/example/trustport_mobile/ui/Adapters/OrderAdapter.java
+++ TrustPort_Mobile/app/src/main/java/com/example/trustport_mobile/ui/Adapters/OrderAdapter.java
@@ -1,14 +1,18 @@
 package com.example.trustport_mobile.ui.Adapters;
 
+import android.content.Intent;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.trustport_mobile.R;
+import com.example.trustport_mobile.ui.Activity.ChitietdonhangActivity;
 import com.example.trustport_mobile.ui.Models.Order;
 
 import java.util.List;
@@ -16,8 +20,10 @@ import java.util.List;
 public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.OrderViewHolder> {
 
     private List<Order> orderList;
+    private Context context;
 
     public OrderAdapter(List<Order> orderList) {
+        this.context = context;
         this.orderList = orderList;
     }
 
@@ -37,6 +43,10 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.OrderViewHol
         holder.address.setText(order.getAddress());
         holder.product.setText(order.getProduct());
         holder.price.setText(order.getPrice());
+
+        holder.detail_button.setOnClickListener(v -> {
+            Intent intent = new Intent(context, ChitietdonhangActivity.class);
+        });
     }
 
     @Override
@@ -46,7 +56,7 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.OrderViewHol
 
     public static class OrderViewHolder extends RecyclerView.ViewHolder {
         TextView orderId, status, senderInfo, address, product, price;
-
+        Button detail_button;
         public OrderViewHolder(@NonNull View itemView) {
             super(itemView);
             orderId = itemView.findViewById(R.id.tv_order_id);
@@ -55,6 +65,7 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.OrderViewHol
             address = itemView.findViewById(R.id.tv_address);
             product = itemView.findViewById(R.id.tv_product);
             price = itemView.findViewById(R.id.tv_price);
+            detail_button = itemView.findViewById(R.id.btn_route);
         }
     }
 }
